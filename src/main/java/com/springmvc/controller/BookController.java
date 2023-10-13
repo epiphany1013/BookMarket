bbfda0a1bcd02b0d5d04c18a09d7ab92332e4231
epiphany1013 <hisoftly@naver.com>
2023-10-13 09:34:25 +0900

    Modify Absolute Path to Relative Path in BookController

--- src/main/java/com/springmvc/controller/BookController.java
+++ src/main/java/com/springmvc/controller/BookController.java
@@ -10,6 +10,7 @@ import com.springmvc.domain.Book;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.ServletContext;
 import java.io.*;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +21,9 @@ import java.util.Set;
 public class BookController {
     @Autowired
     private BookService bookService;
+    @Autowired
+    ServletContext servletContext;
+
 
     @GetMapping
     public String requestBookList(Model model) {
@@ -70,8 +74,10 @@ public class BookController {
         MultipartFile bookImage = book.getBookImage();
 
         String saveName = bookImage.getOriginalFilename();
-        File saveFile = new File("/Users/gh/Documents/code/java/BookMarket/src/main/webapp/resources/images", saveName);
+//        File saveFile = new File("/Users/gh/Documents/code/java/BookMarket/src/main/webapp/resources/images", saveName);
 //        File saveFile = new File("/resources/images", saveName); // java파일에서는 경로를 절대경로로 적어주어야하고, jsp에서는 상대경로로 적어주어야 한다.
+        String rootPath = servletContext.getRealPath("/");
+        File saveFile = new File(rootPath + "resources/images", saveName); //ServletContext를 이용하여 웹 애플리케이션의 루트 경로를 얻을 수 있습니다. 이 경로에 /resources/images를 추가하여 이미지 저장 경로를 구성하는 방법 알아냄
 
         if (bookImage != null && !bookImage.isEmpty()) {
             try (InputStream inputStream = bookImage.getInputStream();
